1989172cd7d2e44f7339b533b782e9671cce492c
Damian Flores <yoman.damian@gmail.com>
2023-06-18 20:43:03 -0300

    modificamos archivo Damian linea 10

--- Damian.py
+++ Damian.py
@@ -5,7 +5,7 @@ numero = int(input("Digite un numero: "))
 if numero>0:
     print("El numero es positivo")
 elif numero==0:
-    print ("El numero es cero")
+    print ("El numero es cero ")
 else:
     print("El numero es negativo")
 
